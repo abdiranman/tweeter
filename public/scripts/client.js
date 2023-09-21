8c75874237bf211dfac6f3f5b58f4f9ca03dc92e
abdiranman <abdirahman.abdi330@.com>
2023-09-21 19:46:45 -0400

    fixed errors and added resposive design

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -28,50 +28,81 @@ const tweetData = [
   }
 ]
 const createTweetElement = (data) => {
-  const $name = $('<header><span>').text(data.user.name);
+  const $header = $('<header>');
+  const $name = $('<span>').text(data.user.name);
+  const $avatar = $(`<img src=${data.user.avatars}></img>`);
   const $handle = $('<span>').text(data.user.handle);
   const $content = $('<p>').text(data.content.text);
-  const $time = $('<footer><div>').text(data.created_at);
+  const $time = $('<footer><div>').text(`${timeago.format(data["created_at"])}`);
   const $icons = $('<div>').append('<i class="fa-solid fa-flag"></i>', '<i class="fa-solid fa-retweet"></i>', '<i class="fa-solid fa-heart"></i>');
-  // Puts $handle in same header as $name
-  $name.append($handle);
+  
+  //puts avatar, handle and name into same footer
+  $name.prepend($avatar);
+  $header.append($name, $handle);
+
   //Puts $icons in same footer as $time
   $time.append($icons);
+
+  //Used to create a tweet
   const $tweet = $('<article class="tweet">');
-  $tweet.append($name, $content, $time);
-  return $tweet;
+  $tweet.append($header, $content, $time);
+    return $tweet;
 }
+
 const renderTweets = (data) => {
   const $tweetComponent = $('#tweets-container');
-  console.log('tweet:', $tweetComponent);
   $tweetComponent.empty();
+
   for(const user of data) {
     const $tweet = createTweetElement(user);
     $tweetComponent.prepend($tweet);
   }
 }
 $(document).ready(function() {
+  const loadTweets = () => {
+    $.ajax({
+      url: '/tweets',
+      method: 'GET',
+      dataType: 'json',
+      success: (tweets) => {
+        renderTweets(tweets);
+
+      },
+      error: (err) => {
+        console.log("error:", err);
+      }
+    });
+  };
+  loadTweets();
+
   $("#submitTweet").submit(function(e){
+    // prevents page from refreshing
     e.preventDefault();
 
-    // slice gets rid of "text="
     const serializedTweet = $(e.target).serialize();
 
+    //slice gets rid of "text="
     if (serializedTweet.slice(5) === "") {
-      return window.alert("Can't post an empty tweet!");
+      const errMsg = "Can't post an empty tweet!"
+      return $(".error").text(errMsg).show().delay(2000).fadeOut();
     }
 
-    if (serializedTweet.slice(5).length > 140) {
-      return window.alert("Your tweet is too long!")
+    if (serializedTweet.slice(5).replace(/%20/g, ' ').length > 140) {
+      const errMsg = "Your tweet is too long!"
+      return $(".error").text(errMsg).show().delay(2000).fadeOut();
     }
 
     $.post('/tweets', serializedTweet, response => {
+      //clears form after post is succesful
+      $("#submitTweet").trigger("reset");
+      //resets counter to 140 after form is cleared
+      $(".counter").text('140');
+      //loads tweets without refreshing
+      loadTweets();
     })
   });
   renderTweets(tweetData);
 });
-
-
 // const createTweetElement = (data) => {
 //     const $header = $('<header>');
 //     const $name = $('<span>').text(data.user.name);
@@ -165,4 +196,3 @@ $(document).ready(function() {
 //     });
   
 //   });
-  
